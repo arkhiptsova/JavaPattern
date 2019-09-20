c3a32abaed08e427943f73df3dd3feb3a27f8f31
Mila Arkhiptsova <40221785+arkhiptsova@users.noreply.github.com>
2019-09-19 22:43:51 -0500

    Add files via upload

---
+++ JavaPattern.java
@@ -0,0 +1,46 @@
+package assignment;
+
+/*
+ Mila Arkhiptcova
+ 08/27/2019
+ Assignment 2.2
+ 
+ This program displays word Java in a specific pattern
+ */
+public class JavaPattern {
+
+	public static void main(String[] args) {
+
+		    //Declare count
+		    int count = 2;
+			
+			//Does for loop until count equals 0
+			while(count > 0)
+			{
+				//Declare i
+				int i = 4;
+				//Displays the variable i until it equals count.
+				while(i != 0)
+				{
+					if(i == 4 || i == 1)
+					{
+						System.out.print("  " + i + " ");
+						System.out.println(i + "  \t \t 11");
+					}
+					else if(i == 3 || i == 2)
+					{
+						System.out.print(i+ " ");
+						System.out.print(i+ " ");
+						System.out.print(i+ " ");
+						System.out.println(i+ " \t 11");
+					}
+					i--;
+				}
+				System.out.println("\t \t 11");
+
+				
+				//Increase count by 1
+				count--;
+			}
+		  }
+		}
